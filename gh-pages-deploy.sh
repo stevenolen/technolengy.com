031e90fbd79ad0ecb6a1129311a8ce794c9a90e5
Steve Nolen <technolengy@gmail.com>
2015-02-15 11:03:21 -0800

    something

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -6,5 +6,5 @@ rm -rf build || exit 0;
  git config user.email "travis@technolengy.com"
  git add .
  git commit -m "Deployed to Github Pages"
- git push --force --quiet "https://${GH_TOKEN}@github.com/stevenolen/technolengy.com" gh-pages > /dev/null 2>&1
+ git push --force --quiet "https://${GH_TOKEN}@github.com/stevenolen/technolengy.com" master:gh-pages > /dev/null 2>&1
 )
