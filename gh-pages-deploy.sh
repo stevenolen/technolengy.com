131759d87929ae5cf919793fc4246758a17b1939
Steve Nolen <technolengy@gmail.com>
2015-02-15 10:54:20 -0800

    travis should push back to github pages

---
+++ gh-pages-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -rf build || exit 0;
+( cd build
+ git init
+ git config user.name "Travis-CI"
+ git config user.email "travis@technolengy.com"
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@github.com/stevenolen/technolengy.com" gh-pages > /dev/null 2>&1
+)
